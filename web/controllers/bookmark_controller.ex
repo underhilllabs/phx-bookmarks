38220652457c3aca05c941215b09b31e02473c2b
Bart Lantz <bartlantz@gmail.com>
2015-07-08 10:08:21 -0600

    adds pagination to bookmark query

--- web/controllers/bookmark_controller.ex
+++ web/controllers/bookmark_controller.ex
@@ -1,17 +1,17 @@
 defmodule PhxBkmark.BookmarkController do
   use PhxBkmark.Web, :controller
   import Ecto.Query, only: [from: 2]
-
+  @per_page 20
   plug :action
 
-  def index(conn, _params) do
-    query = from b in PhxBkmark.Bookmark,
-      order_by: [desc: b.updated_at],
-      limit: 30,
-      offset: 300,
-      select: b
-
-    bookmarks = Repo.all(query) |> Repo.preload [:user, :tags] 
+  def index(conn, %{"page" => page}) do
+    if page do
+      page = String.to_integer(page)
+    else
+      page = 1
+    end
+    
+    bookmarks = bookmark_query |> paginate(page, @per_page) |> Repo.all |> Repo.preload [:user, :tags]
     render(conn, "index.html", bookmarks: bookmarks)
   end
 
@@ -22,5 +22,18 @@ defmodule PhxBkmark.BookmarkController do
   # We will use the params, so no _!
   def create(conn, params) do
     render conn, "index.html"
+  end
+
+  def bookmark_query do
+    from b in PhxBkmark.Bookmark,
+    order_by: [desc: b.updated_at],
+    select: b
+  end
+
+  def paginate(query, page, per_page) do
+    offset = (page - 1) * per_page
+    from query,
+    offset: ^offset,
+    limit: ^per_page
   end
 end
